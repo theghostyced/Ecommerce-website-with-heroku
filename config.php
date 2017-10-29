411e50f5f48b4f476cd06ca19248728f0ec57072
dansky <dan4allu93@gmail.com>
2017-10-29 12:22:42 -0700

    Changed the config file

--- config.php
+++ config.php
@@ -17,10 +17,11 @@ define('DIR_MODIFICATION', '/app/system/storage/modification/');
 define('DIR_UPLOAD', '/app/system/storage/upload/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'oc2');
+$db = parse_url($_ENV["DATABASE_URL"]); 
+define('DB_DRIVER', 'postgresql');
+define('DB_HOSTNAME', $db["host"]);
+define('DB_USERNAME', $db["user"]);
+define('DB_PASSWORD', $db["pass"]);
+define('DB_DATABASE', trim($db["path"],"/"));
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
